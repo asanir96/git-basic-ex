d9567756f5ac159fe8bf49b8b8ab4c23ffd1d6e9
asanir96 <asanir96@gmail.com>
2026-03-29 11:15:17 +0300

    Add gBalls and changed onBallClick to use gBalls

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 'use strict'
 
 var gBallSizeDiff = 50
-var gBallSize = 100
+const gBalls = {
+    ball1: { color: 'burlywood', size: 100 },
+    ball2: { color: 'rgb(135, 222, 180)', size: 100 }
+}
 
-function onBallClick(elBall, maxDiameter) {
-    var randBallColor = getRandomColor()
+function onBallClick(elBall, maxDiameter, ballNum) {
+    const currBall = gBalls[elBall.classList[1]]
+    
     gBallSizeDiff = getRandomInt(20, 61)
     
-    gBallSize += gBallSizeDiff
-    if (gBallSize >= maxDiameter) gBallSize = 100
-
-    elBall.style.width = gBallSize + 'px'
-    elBall.style.height = gBallSize + 'px'
-
-    elBall.innerText = gBallSize
+    currBall.size += gBallSizeDiff
+    if (currBall.size >= maxDiameter) currBall.size = 100
+    
+    elBall.style.width = currBall.size  + 'px'
+    elBall.style.height = currBall.size  + 'px'
+    elBall.innerText = currBall.size 
+    
+    var randBallColor = getRandomColor()
+    currBall.color = randBallColor
     
     elBall.style.backgroundColor = randBallColor
 }
