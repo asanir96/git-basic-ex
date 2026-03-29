2a48b00903d2e9155f8f96f65700df1c4ec283c2
asanir96 <asanir96@gmail.com>
2026-03-29 08:12:41 +0300

    Add ball random color change behavior

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ var gBallSize = 100
 var gBallSizeLimit = 400
 
 function onBallClick(elBall) {
+    var randBallColor = getRandomColor()
     gBallSizeDiff = getRandomInt(20, 61)
+    
     gBallSize += gBallSizeDiff
     if (gBallSize >= gBallSizeLimit) gBallSize = 100
 
@@ -13,4 +15,6 @@ function onBallClick(elBall) {
     elBall.style.height = gBallSize + 'px'
 
     elBall.innerText = gBallSize
+    
+    elBall.style.backgroundColor = randBallColor
 }
