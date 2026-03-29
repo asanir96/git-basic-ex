1425932ef7db0c8556d99b774888f05ba0cbf85d
asanir96 <asanir96@gmail.com>
2026-03-29 07:33:09 +0300

    Revert "Add ball diameter limit and ball transition"
    This reverts commit 56f3fb20419055b063ce5954a090bcf536e9614a.

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@
 
 const gBallSizeDiff = 50
 var gBallSize = 100
-var gBallSizeLimit = 400
 
-function onBallClick(elBall) {
+function onBallClick(elBall){
     gBallSize += gBallSizeDiff
-    if (gBallSize >= gBallSizeLimit) gBallSize = 100
-
 
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
-
+    
     elBall.innerText = gBallSize
 }
