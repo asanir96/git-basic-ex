cb92561f5c24cecaa50366a17caa7e4b03c9a28f
asanir96 <asanir96@gmail.com>
2026-03-29 17:51:20 +0300

    Add fourth ball and implement onReduceBallClick

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function onBallClick(elBall, maxDiameter) {
     const currBall = gBalls[elBall.classList[1]]
 
     const randBallSizeDiff = getRandomInt(20, 61)
-    const newBallSize = gBalls[elBall.classList[1]].size +randBallSizeDiff
+    const newBallSize = gBalls[elBall.classList[1]].size + randBallSizeDiff
     const randBallColor = getRandomColor()
     changeBallSize(elBall, newBallSize, maxDiameter)
     changeBallColor(elBall, randBallColor)
@@ -37,34 +37,26 @@ function onSwapBallClick() {
 
     const ball1Diameter = gBalls.ball1.size
     const ball2Diameter = gBalls.ball2.size
-    
+
     const ball1Color = gBalls.ball1.color
     const ball2Color = gBalls.ball2.color
 
-    changeBallSize(elBall1,ball2Diameter)
-    changeBallColor(elBall1,ball2Color,400)
+    changeBallSize(elBall1, ball2Diameter)
+    changeBallColor(elBall1, ball2Color, 400)
 
-    changeBallSize(elBall2,ball1Diameter,200)
-    changeBallColor(elBall2,ball1Color)
+    changeBallSize(elBall2, ball1Diameter, 200)
+    changeBallColor(elBall2, ball1Color)
+}
 
-    // gBalls.ball1.size = ball2Diameter
-    // gBalls.ball2.size = ball1Diameter
+function onReduceBallClick() {
+    const randBallSizeDiff = getRandomInt(20, 61)
 
-    // elBall1.style.width = ball2Diameter + 'px'
-    // elBall1.style.height = ball2Diameter + 'px'
+    const newBall1Size = gBalls.ball1.size - randBallSizeDiff
+    const newBall2Size = gBalls.ball2.size - randBallSizeDiff
 
-    // elBall2.style.width = ball1Diameter + 'px'
-    // elBall2.style.height = ball1Diameter + 'px'
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
 
-
-    // elBall1.innerText = ball2Diameter
-    // elBall2.innerText = ball1Diameter
-
-
-    // elBall1.style.backgroundColor = ball2Color
-    // elBall2.style.backgroundColor = ball1Color
-
-    // gBalls.ball1.color = ball2Color
-    // gBalls.ball2.color = ball1Color
-
+    changeBallSize(elBall1, newBall1Size, undefined, 100)
+    changeBallSize(elBall2, newBall2Size, undefined, 100)
 }
